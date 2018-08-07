52e9bcec79afb13ce98f0e06f4d10f451f358fd9
Ishtar Perez <isperez@csumb.edu>
2018-08-07 09:22:43 +0000

    Blanca

--- program1/Numbers.php
+++ program1/Numbers.php
@@ -1,19 +1,53 @@
 <?php
+
+
+function contains($arr,$size,$target) {
+    for($i = 0; $i < $size; $i++) {
+        if($arr[$i] == $target) {
+            return true;
+        }
+    }
+    return false;
+}
+
+
 // Create 5 random numbers to be displayed
 function numbers() {
-for($i = 0; $i < 5; $i++ ) {
-    echo " " , (rand(1,70));
-    }
-}
 
+
+// loop 5 times
+
+$nums = array(0,0,0,0,0);
+$size = 1;
+$nums[0] = rand(1,69);
+    for ($i =  1; $i < 5; $i++) {
+        $num = rand(1,69);
+        while(contains($nums,$size,$num)) {
+            $num = rand(1,69);
+        }
+        
+        $nums[$size] = $num;
+        $size += 1;
+    }
+    
+    for($i = 0; $i < 5; $i++) {
+        echo " " . $nums[$i];
+    }
+    
+    
+}
+/*for($i = 0; $i < 5; $i++ ) {
+    echo " " , (rand(1,69));
+    
+    
+    }
+}*/
 // Create 1 random Number
 function oneNumber() {
-    echo (rand(1,30));
+    echo (rand(1,29));
    
 }
-
 ?>
-
 <!DOCTYPE html>
 <html>
     <head>
@@ -36,43 +70,87 @@ function oneNumber() {
             #box {
                
                 text-align:center;
+                padding:15px;
             }
-            button {
-                background-color:aqua;
+           
+            table{
+                border-collapse: collapse;
+                width: 50%;
+                display:inline;
+               
+              
             }
+            td, th {
+                border: 1px solid black;
+                text-align: center;
+                padding: 8px;
+                background-color:pink;
+            }
+           
+            
             
         </style>
         
         <script>
                 
                 $(document).ready(function(){
-            $("box").click(function(){
+                $("box").click(function(){
                 //alert("The paragraph was clicked.");
             });
         });
         </script>
-        
     </head>
         
     <body>
         <h1>Lucky Numbers</h1>
         
         <div id="num" >
-            5 Random Numbers:
-            <?php Numbers(); ?> 
+        <table>
+        <tr>
+            <th>5 Random Numbers</th>
+            <br><th> 1 Random Number</th>
+            
+        </tr>
+        <!-- Create a table with 5 different set of random numbers -->
+            <td><?php Numbers(); ?></td>
+            <td><?php oneNumber(); ?></td>
+        <tr>
+            <td><?php Numbers(); ?></td>
+            <td><?php oneNumber(); ?></td>
+            
+        </tr>  
+        
+        <tr>
+            <td><?php Numbers(); ?></td>
+            <td><?php oneNumber(); ?></td>
+            
+        </tr>
+            <td><?php Numbers(); ?></td>
+            <td><?php oneNumber(); ?></td>
+            
+        <tr>
+            
+        </tr>
+            <td><?php Numbers(); ?></td>
+            <td><?php oneNumber(); ?></td>
+        
+        <tr>
+            
+        </tr>
+            
+           
+           
+        
+        <tr>
+
+            
+        </tr>
+        </table>
         </div> 
         
-        <br>
-        
-        <div id="oneNum">
-            1 Random Number: 
-            <?php oneNumber(); ?>
-        </div>
-        
-        <br>
+    
         
         <form id="box">
-            
              <button type="submit" class="btn btn-danger">New Numbers</button>
         </form>
         
